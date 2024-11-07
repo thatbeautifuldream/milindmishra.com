9873f0dccf43925c309b369c02930bcc6b75eec5
Milind Mishra <milind.mishra4@gmail.com>
2024-11-07 10:44:42 +0530

    feat: add rewrites

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,26 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   rewrites: async () => [
     {
+      source: "/li",
+      destination: "https://www.linkedin.com/in/mishramilind/",
+    },
+    {
+      source: "/gh",
+      destination: "https://github.com/thatbeautifuldream",
+    },
+    {
+      source: "/x",
+      destination: "https://twitter.com/milindmishra_",
+    },
+    {
+      source: "/ig",
+      destination: "https://www.instagram.com/thatbeautifuldream",
+    },
+    {
+      source: "/bsky",
+      destination: "https://bsky.app/profile/milindmishra.bsky.social",
+    },
+    {
       source: "/resume",
       destination:
         "https://cdn.jsdelivr.net/gh/thatbeautifuldream/resume-cron/resume.pdf",
