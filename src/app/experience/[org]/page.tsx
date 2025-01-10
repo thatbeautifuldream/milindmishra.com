a1ef28cdcf405bbf75154a9fc981cfdf77c397c5
Milind Mishra <milind.mishra4@gmail.com>
2025-01-10 22:17:49 +0530

    Remove unused imports

---
+++ src/app/experience/[org]/page.tsx
@@ -0,0 +1,36 @@
+import { experience } from "@/data/resume";
+import { formatDate } from "@/lib/utils";
+
+export default async function ExperiencePage({
+  params,
+}: {
+  params: { org: string };
+}) {
+  const { org } = params;
+
+  const job = experience.find(
+    (job) => job.organization.name.toLowerCase() === org
+  );
+
+  if (!job) {
+    return <p>Experience not found</p>;
+  }
+
+  return (
+    <section className="~space-y-8/6">
+      <h2 className="~text-2xl/3xl font-bold">{job.title}</h2>
+      <div className="text-green-300 flex items-center ~text-sm/base">
+        <p>{job.organization.name}</p>
+        <p>{job.location}</p>
+      </div>
+      {job.start_date && (
+        <p className="text-green-300 ~text-xs/sm mb-2 sm:mb-3 ~mt-1/2">
+          {formatDate(job.start_date)} - {formatDate(job.end_date)}
+        </p>
+      )}
+      <p className="text-green-300 ~text-sm/base leading-relaxed">
+        {job.description}
+      </p>
+    </section>
+  );
+}
