679b4a67437412fb12f7b58d49d8260881797232
Milind Mishra <milind.mishra4@gmail.com>
2025-03-08 12:03:10 +0530

    feat: update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,21 +1,19 @@
 import React from "react";
-import Icons from "./icons";
 
-const navigation = [
+const navigation: { name: string; href: URL }[] = [
   {
     name: "GitHub",
-    href: "https://github.com/thatbeautifuldream?ref=milindmishra.com",
-    icon: Icons.Github,
+    href: new URL("https://github.com/thatbeautifuldream?ref=milindmishra.com"),
   },
   {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/mishramilind/?ref=milindmishra.com",
-    icon: Icons.LinkedIn,
+    href: new URL(
+      "https://www.linkedin.com/in/mishramilind/?ref=milindmishra.com"
+    ),
   },
   {
     name: "Email",
-    href: "mailto:milind.mishra4@gmail.com",
-    icon: Icons.Gmail,
+    href: new URL("mailto:milind.mishra4@gmail.com"),
   },
 ];
 
@@ -27,22 +25,21 @@ export function Footer() {
       id="contact"
       className="py-12 border-t border-dashed border-green-600/50 select-none"
     >
-      <div className="mx-auto max-w-6xl px-6 md:flex md:items-center md:justify-between lg:px-8">
+      <div className="mx-auto max-w-6xl px-6 md:flex md:items-center md:justify-between lg:px-8 font-mono">
         <div className="flex justify-center gap-x-6 md:order-2">
           {navigation.map((item) => (
             <a
               key={item.name}
-              href={item.href}
-              className="text-gray-400 hover:text-green-300 transition-colors"
+              href={item.href.toString()}
+              className="text-green-400 hover:text-green-300 transition-colors"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <span className="sr-only">{item.name}</span>
-              <item.icon className="size-6" aria-hidden="true" />
+              <span className="text-xs">{item.name}</span>
             </a>
           ))}
         </div>
-        <p className="mt-8 text-center text-sm/6 text-gray-400 hover:text-green-300 transition-colors md:order-1 md:mt-0">
+        <p className="mt-8 text-center text-sm/6 text-green-400 hover:text-green-300 transition-colors md:order-1 md:mt-0">
           © {currentYear} Milind Mishra.
         </p>
       </div>
