b7bb2293b8a18f3980ceeca34fdd1df7d1fabf83
Milind Mishra <milind.mishra4@gmail.com>
2025-04-11 20:48:48 +0530

    refactor: simplify font imports by removing unused fonts

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,35 +1,11 @@
-import {
-  Inter,
-  Space_Grotesk,
-  Instrument_Serif,
-  Bricolage_Grotesque,
-  Libre_Baskerville,
-} from "next/font/google";
+import { Inter, Bricolage_Grotesque } from "next/font/google";
 
 export const inter = Inter({
   subsets: ["latin"],
   display: "swap",
 });
 
-export const spaceGrotesk = Space_Grotesk({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-  display: "swap",
-});
-
-export const instrumentSerif = Instrument_Serif({
-  subsets: ["latin"],
-  weight: ["400"],
-  display: "swap",
-});
-
 export const bricolageGrotesque = Bricolage_Grotesque({
   subsets: ["latin"],
-  display: "swap",
-});
-
-export const libreBaskerville = Libre_Baskerville({
-  subsets: ["latin"],
-  weight: ["400", "700"],
   display: "swap",
 });
