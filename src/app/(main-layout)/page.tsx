9384d0257d4f5f0b7687ee95ba031804cc280cd0
Milind Mishra <milind.mishra4@gmail.com>
2025-05-31 05:20:57 +0530

    feat: add gh cal

--- src/app/(main-layout)/page.tsx
+++ src/app/(main-layout)/page.tsx
@@ -6,6 +6,7 @@ import { Projects } from "@/components/projects";
 import { Skills } from "@/components/skills";
 import { Testimonials } from "@/components/testimonials";
 import { testimonials } from "@/data/resume";
+import GitHubCalendar from "react-github-calendar";
 
 export default function Home() {
   return (
@@ -13,6 +14,9 @@ export default function Home() {
       <div className="~space-y-12/16">
         <Hero />
         <Experience />
+        <div className="flex justify-center">
+          <GitHubCalendar username="thatbeautifuldream" />
+        </div>
         <Projects />
         <Testimonials testimonials={testimonials} />
         <Skills />
